71b009962a979336eac49834f9df3540b15f8d88
Afthab <mohammedafthab15@gmail.com>
2023-05-31 13:10:39 +0530

    config file and database connection has been intialized

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	config "github.com/auth/service/pkg/config"
+	db "github.com/auth/service/pkg/db"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalln("Failed to load the Congiguration File: ", err)
+		return
+	}
+	DB, err := db.ConnectToDb(cfg)
+	if err != nil {
+		log.Fatalln("Failed to connect to the Database: ", err)
+		return
+	}
+	fmt.Println(DB)
+
+}
--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -1,1 +1,21 @@
 package db
+
+import (
+	"fmt"
+
+	config "github.com/auth/service/pkg/config"
+	domain "github.com/auth/service/pkg/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectToDb(cfg config.Config) (*gorm.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+	DB, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+	DB.AutoMigrate(&domain.User{})
+
+	return DB, dbErr
+
+}
